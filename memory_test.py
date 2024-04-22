b8ea5647646a7cd273734f7c3a68a87fad8d9608
Noha Shehab <63148220+NohaAShehab@users.noreply.github.com>
2024-04-22 09:50:54 +0200

    sets and dicts

---
+++ memory_test.py
@@ -0,0 +1,14 @@
+
+mys = {"Ahmed", "Zeinab", "iti", 3234, "Ahmed",
+       "Mohamed", "Mohamed","Mohamed", 3.12, True, 0 }
+
+print(mys)
+
+
+### info
+
+# info = ["noha", 31, "iti"] # unlabeled datatype
+#
+# print(info)
+
+# need new datatype --> represent key:value pair
